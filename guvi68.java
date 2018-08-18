615f558a59a3836ea78ee017cf3f89e80d314095
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-08-18 11:55:57 +0530

    Create guvi68.java

---
+++ guvi68.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter n value");
+		n=sc.nextInt();
+		if(n%7==0)
+		
+			System.out.println("yes");
+		else
+		System.out.println("no");
+		
+		}
+	}
